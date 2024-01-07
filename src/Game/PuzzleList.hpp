d4b12265803e8c279fb390468a9ea6083b8e21b2
BJMinhNhut <nhutben678@gmail.com>
2024-01-07 23:58:51 +0700

    Move Puzzle List to Context

---
+++ src/Game/PuzzleList.hpp
@@ -0,0 +1,37 @@
+//
+// Created by MINH NHUT on 1/7/2024.
+//
+
+#ifndef CHESS_CHILL_PUZZLELIST_HPP
+#define CHESS_CHILL_PUZZLELIST_HPP
+
+#include "Puzzle.hpp"
+
+class PuzzleList {
+   public:
+	static const std::string PATH;
+
+   public:
+	PuzzleList();
+
+	void load();
+	void save() const;
+
+	void nextPuzzle();
+	void prevPuzzle();
+	void setPuzzle(int index);
+
+	[[nodiscard]] Puzzle& getChosenPuzzle();
+	[[nodiscard]] const Puzzle& getChosenPuzzle() const;
+
+	[[nodiscard]] Puzzle& getPuzzle(int index);
+	[[nodiscard]] const Puzzle& getPuzzle(int index) const;
+
+	[[nodiscard]] size_t size() const;
+
+   private:
+	std::vector<Puzzle> mPuzzles;
+	int mIndex;
+};
+
+#endif  //CHESS_CHILL_PUZZLELIST_HPP
